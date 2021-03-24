8ea19520fd777815123b18b77c364533cb9c8f62
Faiqzakii <221709674@stis.ac.id>
2021-03-24 06:52:52 +0000

    add main app

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+
+import mediapipe as mp
+import cv2
+
+import displayer
+from camera import CameraStream
+from video import VideoStream
+from trainer import train_model_from_videos
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Sign to text: Command that parse a video stream and recognizes signs')
+    parser.add_argument("-v", "--video", type=str, nargs='?', help='video input for testing')
+    parser.add_argument("-t", '--train', action="store_true", help='train or test')
+    parser.add_argument("-n", "--no-evaluate", action="store_true")
+    args = parser.parse_args()
+
+    # init stream
+    if args.video:
+        stream = VideoStream(args.video)
+    else:
+        stream = CameraStream()
+
+    # init components (hand landmark and pose landmark)
+    mp_hands = mp.solutions.hands
+    mp_pose = mp.solutions.pose
+
+    # launch action
+    if args.no_evaluate:
+        displayer.display_from_stream(stream, mp_pose, mp_hands)
+    if args.train:
+        train_model_from_videos()
+    else:
+        displayer.display_evaluate_from_stream(stream, mp_pose, mp_hands)
--- tes.py
+++ tes.py
@@ -1,10 +1,41 @@
-import argparse
+import cv2
+import mediapipe as mp
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Sign to text: Command that parse a video stream and recognizes signs')
-    parser.add_argument("-v", "--video", type=str, nargs='?')
-    parser.add_argument("-t", '--train', action="store_true")
-    parser.add_argument("-n", "--no-evaluate", action="store_true")
-    args = parser.parse_args()
+def run_simple():
+    mp_hands = mp.solutions.hands
+    mp_pose = mp.solutions.pose
+    mp_drawing = mp.solutions.drawing_utils
+    hands = mp_hands.Hands(
+        min_detection_confidence=0.7, min_tracking_confidence=0.5)
+    pose = mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.5)
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            break
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(image)
+        results_pose = pose.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmark in results.multi_hand_landmarks:
+                print(hand_landmark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP])
+            
+            #print(results.multi_hand_landmarks)
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        if results_pose.pose_landmarks:
+            mp_drawing.draw_landmarks(
+                image, results_pose.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        cv2.imshow('MediaPipe Hands', image)
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+    hands.close()
+    pose.close()
+    cap.release()
 
-    print(args)
+
+run_simple()
